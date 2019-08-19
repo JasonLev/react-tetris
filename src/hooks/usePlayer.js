231604fd3a817713adc6011b57cfa32634bed3a8
JasonLev <jasond.levy@gmail.com>
2019-08-19 15:10:18 -0700

    create rotating for tetrominos (with collision detection)

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,7 +1,7 @@
 import { useState, useCallback } from "react";
 
 import { randomTetromino, TETROMINOS } from "../tetrominos";
-import { STAGE_WIDTH } from "../gameHelpers";
+import { STAGE_WIDTH, checkCollision } from "../gameHelpers";
 
 export const usePlayer = () => {
     const [player, setPlayer] = useState({
@@ -9,6 +9,37 @@ export const usePlayer = () => {
         tetromino: TETROMINOS[0].shape,
         collided: false
     })
+
+    const rotate = (matrix, dir) => {
+        // make the rows to become columns (transpose)
+        const rotatedTetromino = matrix.map((_, index) => 
+            matrix.map(col => col[index])
+        );
+        // reverse each row to rotate the matrix
+        if (dir > 0) {
+            return rotatedTetromino.map(row => row.reverse());
+        }
+        return rotatedTetromino.reverse();
+    }
+    const playerRotate = (stage, dir) => {
+        const playerClone = JSON.parse(JSON.stringify(player));
+        playerClone.tetromino = rotate(playerClone.tetromino, dir);
+        // don't allow rotate when beside a boundary
+        const pos = playerClone.pos.x
+        let offset = 1;
+        while (checkCollision(playerClone, stage, { x: 0, y: 0 })) {
+            playerClone.pos.x += offset;
+            offset = -(offset + (offset > 0 ? 1 : -1));
+            if (offset > playerClone.tetromino[0].length) {
+                rotate(playerClone.tetromino, -dir);
+                playerClone.pos.x = pos;
+                return;
+            }
+        }
+        
+        
+        setPlayer(playerClone);
+    }
     const updatePlayerPos = ({ x, y, collided}) => {
         setPlayer(prev => ({
             ...prev,
@@ -24,5 +55,5 @@ export const usePlayer = () => {
       });
     }, []);
 
-    return [player, updatePlayerPos, resetPlayer];
+    return [player, updatePlayerPos, resetPlayer, playerRotate];
 }
